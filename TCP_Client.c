0da0cbf0770db8e3ffe1446d60dbd8d904aa68e0
Ponce-1 <37884033+Ponce-1@users.noreply.github.com>
2019-04-22 12:18:34 -0700

    Add files via upload

--- TCP_Client.c
+++ TCP_Client.c
@@ -7,6 +7,7 @@
 #include <sys/types.h> 
 #define PORT 5000 
 #define MAXLINE 1024 
+#define MAXRECVSTRING 255  /* Longest string to receive */
 void TCP_CALL(){
 	int sockfd; 
 	char buffer[MAXLINE]; 
@@ -69,12 +70,53 @@ while(1){
 	//return 0; 
 }
 //copy to here from code he posted reviever
-//void Broadcast__(
-//	int sock;
-//	struct
-//	braodcast
-//}
-int main() { 
+void Broadcast_reviever_call(int argc, char *argv[]){
+	int sock;                         /* Socket */
+	struct sockaddr_in broadcastAddr; /* Broadcast Address */
+	unsigned short broadcastPort;     /* Port */
+	char recvString[MAXRECVSTRING+1]; /* Buffer for received string */
+	int recvStringLen;                /* Length of received string */
+
+	if (argc != 2)    /* Test for correct number of arguments */
+	{
+	fprintf(stderr,"Usage: %s <Broadcast Port>\n", argv[0]);
+	exit(1);
+	}
+
+	broadcastPort = atoi(argv[1]);   /* First arg: broadcast port */
+
+	/* Create a best-effort datagram socket using UDP */
+	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+	{
+	printf("Socket() failed\n");
+	//DieWithError("socket() failed");
+	}
+	/* Construct bind structure */
+	memset(&broadcastAddr, 0, sizeof(broadcastAddr));   /* Zero out structure */
+	broadcastAddr.sin_family = AF_INET;                 /* Internet address family */
+	broadcastAddr.sin_addr.s_addr = htonl(INADDR_ANY);  /* Any incoming interface */
+	broadcastAddr.sin_port = htons(broadcastPort);      /* Broadcast port */
+
+	/* Bind to the broadcast port */
+	if (bind(sock, (struct sockaddr *) &broadcastAddr, sizeof(broadcastAddr)) < 0)
+	{
+	//DieWithError("bind() failed");
+	printf("bind() failed");
+	}
+	/* Receive a single datagram from the server */
+	if ((recvStringLen = recvfrom(sock, recvString, MAXRECVSTRING, 0, NULL, 0)) < 0)
+	{
+	printf("recvfrom() failed");
+	}
+	//DieWithError("recvfrom() failed");
+
+	recvString[recvStringLen] = '\0';
+	printf("Received: %s\n", recvString);    /* Print the received string */
+
+	close(sock);
+	exit(0);
+}
+int main(int argc, char *argv[]) { 
 	pthread_t threads[2];
 	struct sockaddr_in servaddr, cli;
 	int rc;
